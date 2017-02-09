dffff99787828367fe4475c53e8de63104b1d501
kokardy <2720900+kokardy@users.noreply.github.com>
2017-02-09 13:14:38 +0900

    libreoffice setting

--- assets/run.sh
+++ assets/run.sh
@@ -3,4 +3,11 @@
 
 python /app/init.py
 
+##################################
+#LIBRE OFFICE SETTING
+cd /opt/alfresco/libreoffice/scripts
+ln -s libreoffice_ctl.sh ctl.sh
+sed -i -e "s/\\\\;/;/g" libreoffice_ctl.sh
+####################################
+
 exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
