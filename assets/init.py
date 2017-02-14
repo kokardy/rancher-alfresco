f5cbc9e152603976a038902ae64b54e62cb27f76
Kota Kokado <kouta-k@mbm.nifty.com>
2017-02-14 16:37:47 +0900

    add env CIFS_PORT

--- assets/init.py
+++ assets/init.py
@@ -173,7 +173,7 @@ class ServiceRun():
 
 
 
-  def set_cifs(self, enable, server_name, domain):
+  def set_cifs(self, enable, server_name, domain, port):
       global ALFRESCO_PATH
       global GLOBAL_PROP
 
@@ -189,6 +189,7 @@ class ServiceRun():
           self.replace_all(ALFRESCO_PATH + GLOBAL_PROP, '^#.cifs.hostannounce\s*=.*', 'cifs.hostannounce=true')
           self.replace_all(ALFRESCO_PATH + GLOBAL_PROP, '^#.cifs.broadcast\s*=.*', 'cifs.broadcast=0.0.0.255')
           self.replace_all(ALFRESCO_PATH + GLOBAL_PROP, '^#.cifs.ipv6.enabled\s*=.*', 'cifs.ipv6.enabled=false')
+          self.replace_all(ALFRESCO_PATH + GLOBAL_PROP, '^#.cifs.tcpipSMB.port\s*=.*', 'cifs.tcpipSMB.port='+ port)
       else:
           self.replace_all(ALFRESCO_PATH + GLOBAL_PROP, '^#.cifs.enabled\s*=', '#cifs.enabled=')
           self.replace_all(ALFRESCO_PATH + GLOBAL_PROP, '^#.cifs.Server.Name\s*=', '#cifs.Server.Name=')
@@ -529,7 +530,7 @@ if __name__ == '__main__':
     serviceRun.set_mail(os.getenv('MAIL_HOST', 'localhost'), os.getenv('MAIL_PORT', '25'), os.getenv('MAIL_USER'), os.getenv('MAIL_PASSWORD'), os.getenv('MAIL_PROTOCOL', 'smtp'), os.getenv('MAIL_STARTTLS_ENABLE', 'false'), os.getenv('MAIL_SENDER', 'alfresco@alfresco.org'))
 
     # We set CIFS
-    serviceRun.set_cifs(os.getenv('CIFS_ENABLED', 'true'), os.getenv('CIFS_SERVER_NAME', 'localhost'), os.getenv('CIFS_DOMAIN', 'WORKGROUP'))
+    serviceRun.set_cifs(os.getenv('CIFS_ENABLED', 'true'), os.getenv('CIFS_SERVER_NAME', 'localhost'), os.getenv('CIFS_DOMAIN', 'WORKGROUP'), os.getenv('CIFS_PORT', '445'))
 
     # We set LDAP
     serviceRun.set_ldap(os.getenv('LDAP_ENABLED', 'false'), os.getenv('LDAP_AUTH_FORMAT'), os.getenv('LDAP_HOST'), os.getenv('LDAP_USER'), os.getenv('LDAP_PASSWORD'), os.getenv('LDAP_ADMINS'), os.getenv('LDAP_GROUP_SEARCHBASE'), os.getenv('LDAP_USER_SEARCHBASE'), os.getenv('LDAP_SYNC', 'false'), os.getenv('LDAP_TIMEFORMAT'))
